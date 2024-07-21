382a33fd492d0eac0facff13fb0edf8e6c96515a
Marco Marassi <16927402+thtg88@users.noreply.github.com>
2024-07-21 23:33:17 +0100

    Added sequential cmd

---
+++ cmd/1brc-sequential/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/thtg88/1brc/internal/configs"
+	"github.com/thtg88/1brc/internal/resultswriters"
+	"github.com/thtg88/1brc/internal/solvers/sequentialsolver"
+)
+
+func main() {
+	config := configs.NewDefaultSolverConfig()
+	logger := log.New(os.Stdout, "", log.Lshortfile)
+	csvResultsWriter := resultswriters.NewCSVWriter(config.DestinationFilePath)
+	solver := sequentialsolver.NewSequentialSolver(config, csvResultsWriter, logger)
+
+	solver.Start()
+}
