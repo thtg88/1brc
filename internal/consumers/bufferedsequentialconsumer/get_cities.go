8e81f8a1ac044f95f9b958d111cd5bbcb21d666d
Marco Marassi <16927402+thtg88@users.noreply.github.com>
2024-07-25 13:52:28 +0100

    Allocate exact number of cities on GetCities

--- internal/consumers/bufferedsequentialconsumer/get_cities.go
+++ internal/consumers/bufferedsequentialconsumer/get_cities.go
@@ -4,9 +4,11 @@ func (bsc *BufferedSequentialConsumer) GetCities() []string {
 	// No need to call GetStats here as we only need the key and are not interested in the average temp
 	stats := bsc.Stats
 
-	cities := make([]string, 0, len(stats))
+	cities := make([]string, len(stats))
+	idx := 0
 	for k := range stats {
-		cities = append(cities, k)
+		cities[idx] = k
+		idx++
 	}
 
 	return cities
--- internal/consumers/sequentialconsumer/get_cities.go
+++ internal/consumers/sequentialconsumer/get_cities.go
@@ -4,9 +4,11 @@ func (sc *SequentialConsumer) GetCities() []string {
 	// No need to call GetStats here as we only need the key and are not interested in the average temp
 	stats := sc.Stats
 
-	cities := make([]string, 0, len(stats))
+	cities := make([]string, len(stats))
+	idx := 0
 	for k := range stats {
-		cities = append(cities, k)
+		cities[idx] = k
+		idx++
 	}
 
 	return cities
