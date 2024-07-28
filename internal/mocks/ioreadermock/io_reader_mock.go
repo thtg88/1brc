00e3ee0e57cce666f066bfa9f0f966b3824a0a54
Marco Marassi <16927402+thtg88@users.noreply.github.com>
2024-07-28 20:31:58 +0100

    Added Seek implementation to IO reader mock

--- internal/mocks/ioreadermock/io_reader_mock.go
+++ internal/mocks/ioreadermock/io_reader_mock.go
@@ -2,6 +2,7 @@ package ioreadermock
 
 import (
 	"errors"
+	"fmt"
 	"io"
 )
 
@@ -28,6 +29,7 @@ func (irm *IOReaderMock) Read(p []byte) (n int, err error) {
 	if irm.forceError {
 		return 0, errors.New("forced error on IO reader")
 	}
+
 	remainingBytesCount := len(irm.bytes) - int(irm.position)
 	if len(p) == 0 {
 		n = remainingBytesCount
@@ -42,4 +44,37 @@ func (irm *IOReaderMock) Read(p []byte) (n int, err error) {
 	irm.position = uint64(endPosition)
 
 	return n, nil
+}
+
+func (irm *IOReaderMock) Seek(offset int64, whence int) (int64, error) {
+	if irm.forceError {
+		return 0, errors.New("forced error")
+	}
+
+	var newPosition int64
+
+	length := int64(len(irm.bytes))
+
+	switch whence {
+	case io.SeekStart:
+		newPosition = offset
+	case io.SeekCurrent:
+		newPosition = int64(irm.position) + offset
+	case io.SeekEnd:
+		newPosition = length + offset
+	default:
+		return 0, fmt.Errorf("invalid whence %d", whence)
+	}
+
+	if newPosition >= length {
+		return 0, fmt.Errorf("newPosition %d greater than length %d", newPosition, length)
+	}
+
+	if newPosition < 0 {
+		return 0, fmt.Errorf("newPosition %d less than 0", newPosition)
+	}
+
+	irm.position = uint64(newPosition)
+
+	return 0, nil
 }
